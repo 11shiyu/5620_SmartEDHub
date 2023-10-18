d5e3493909357235024c9b0d24aab1e826b1d050
Shiyu Liu <shiyuliu0613@gmail.com>
2023-10-19 00:23:46 +1100

    update profilestudent UI

--- smarthub/src/containers/ProfileStudent.js
+++ smarthub/src/containers/ProfileStudent.js
@@ -9,33 +9,42 @@ function ProfileStudent({ studentData }) {
   return (
     <div style={{ padding: '20px' }}>
       <div style={{ display: 'flex', flexDirection: 'row' }}>
-        
-
         <Col>
           <Row>
-            <Card style={{ width: '18rem', marginRight: '20px' }}>
-              <Card.Img variant="top" src={studentImage} />
-              <Card.Body>
-                <Card.Title>student name</Card.Title>
-              </Card.Body>
-            </Card>
-            <div>
-              <h5>id</h5>
-              <p>student ID</p>
-              <h5>name</h5>
-              <p>student name</p>
-              {/* 更多的学生信息 */}
-            </div>
-          </Row>
-          <Row>
-            <Button variant="danger" onClick={() => navigate('/Favourite')}>
-                Favourite
-              </Button>
-              <Button variant="danger" onClick={() => navigate('/CorrectionBook')} style={{ marginTop: '10px' }}>
-                Correction Book
-              </Button>
+            {/* Card for student image */}
+            <Col md={5}>
+              <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+                <Card style={{ width: '20rem', marginLeft: '80px' }}>
+                  <Card.Img variant="top" src={studentImage} />
+                  <Card.Body>
+                    <Card.Title>student name</Card.Title>
+                  </Card.Body>
+                </Card>
+              </div>
+            </Col>
+
+            {/* Col for student information */}
+            <Col md={1}>
+              <div>
+                <h5>id</h5>
+                <p>student ID</p>
+                <h5>name</h5>
+                <p>student name</p>
+                <h5>email</h5>
+                <p>studentname@uni.sydney.edu.au</p>
+                {/* 更多的学生信息 */}
+              </div>
+            </Col>
           </Row>
 
+          <Row>
+            <Button variant="danger" onClick={() => navigate('/Favourite')}>
+              Favourite
+            </Button>
+            <Button variant="danger" onClick={() => navigate('/CorrectionBook')} style={{ marginTop: '10px' }}>
+              Correction Book
+            </Button>
+          </Row>
         </Col>
       </div>
     </div>
