681b5fcfaed965b49c8903801cc92ebdeec22f81
Zhixu Wan <zwan9784@uni.sydney.edu.au>
2023-10-28 17:48:22 +1100

    answer list

--- smarthub/src/components/AnswerList.js
+++ smarthub/src/components/AnswerList.js
@@ -2,10 +2,23 @@ import { useState } from 'react';
 import { Card, Button } from 'react-bootstrap';
 import EditStudentsModal from './EditStudentsModal';
 
-function AnswerList() {
+function AnswerList({ansListData, setAnsListData}) {
+
+
     return (
         <div>
-            
+            {ansListData.map((answerItem) => (
+                <Card key={answerItem.studentId} style={{ marginBottom: '20px' }}>
+                    <Card.Body>
+                        <Card.Title>{answerItem.classname}</Card.Title>
+                        <Card.Text>
+                            <strong>Question ID: </strong>{answerItem.questionId} <br/>
+                            <strong>Student ID: </strong>{answerItem.studentId} <br/>
+                        </Card.Text>
+                        <Button variant='primary'>Mark</Button>
+                    </Card.Body>
+                </Card>
+            ))}
         </div>
     );
 }
--- smarthub/src/containers/Grading.js
+++ smarthub/src/containers/Grading.js
@@ -5,13 +5,46 @@ import AnswerList from '../components/AnswerList';
 
 function Grading() {
     const navigate = useNavigate();
+    const teacherUsername = JSON.parse(sessionStorage.getItem('teacherInfo')).username;
 
     const handleBackClick = () => {
         navigate('/ProfileTeacher'); 
     };
 
-    useEffect(() => {
+    const [ansListData, setAnsListData] = useState([]);
 
+    const fetchAnswerList = async (teacher_username) => {
+        try {
+            const response = await fetch(`http://localhost:8090/question/teacherGetAnsweredList?teacher_username=${teacher_username}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': sessionStorage.getItem("tokenStr")
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const responseData = await response.json();
+            const answerListData = await Promise.all(responseData.data.map (async (answerItem) => { 
+                return {
+                    questionStudentId: answerItem.questionStudentId,
+                    questionId: answerItem.questionId,
+                    studentId: answerItem.studentId,
+                    classname: answerItem.classname,
+                    questionAnswer: answerItem.questionAnswer
+                };
+            }));
+            setAnsListData(answerListData);
+
+        } catch (error) {
+            console.error('Failed to fetch answer list:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchAnswerList(teacherUsername);
     }, []);
 
     return (
@@ -26,9 +59,7 @@ function Grading() {
                     <Col md={{span: 8, offset: 1}} >
                         <div className='GradingList'>
                             <h2>Grading List</h2>
-                            <Form>
-
-                            </Form>
+                            <AnswerList ansListData={ansListData} setAnsListData={setAnsListData}/>
                         </div>
                     </Col>
                 </Row>
